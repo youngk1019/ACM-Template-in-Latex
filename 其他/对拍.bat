1af4a63437239ef119acb55645a69e58ec714ac3
youngk1019 <1186354278@qq.com>
2021-09-20 21:54:30 +0800

    更新对拍

--- 其他/对拍.bat
+++ 其他/对拍.bat
@@ -1,9 +1,12 @@
 @echo off
+set cnt=1
 :loop
-gen.exe
-1.exe
-2.exe
+gen.exe > 1.in
+1.exe < 1.in > 1.out
+2.exe < 1.in > 2.out
+echo Cnt:%cnt%
 fc 1.out 2.out
+set /a cnt=%cnt%+1
 if not errorlevel 1 goto loop
 pause
-:endl
+endl
