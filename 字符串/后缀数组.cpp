7491158ee647b34616f46619ffd20d6b12e69173
youngk1019 <1186354278@qq.com>
2022-03-16 15:14:52 +0800

    增加后缀数组

--- 字符串/后缀数组.cpp
+++ 字符串/后缀数组.cpp
@@ -1,37 +1,47 @@
-//Lougu P5410 【模板】扩展KMP（Z函数）
-//b的z函数数组z，即b与b的每一个后缀的LCP长度。
-//b与a的每一个后缀的LCP长度数组p
-//LCP: 最长公共前缀
+//Luogu P3809 【模板】后缀排序
+//SA 为这个字符串的所有非空后缀按字典序从小到大排序，然后按顺序输出后缀的第一个字符在原串中的位置
+//height 为LCP(sa[i],sa[i-1])
 #include <bits/stdc++.h>
 using namespace std;
-#define maxn 20000100
-char a[maxn], b[maxn];
-int z[maxn], p[maxn], n, m;
-void Z(char *s, int n) {
-    z[1] = n;
-    for (int i = 2, l = 0, r = 0; i <= n; i++) {
-        if (i <= r) z[i] = min(z[i - l + 1], r - i + 1);
-        while (i + z[i] <= n && s[i + z[i]] == s[z[i] + 1]) z[i]++;
-        if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+#define maxn 1000100
+int n, m, x[maxn], y[maxn], cnt[maxn], sa[maxn], rk[maxn], height[maxn];
+char s[maxn];
+void get_SA() {
+    n = strlen(s + 1), m = 'z';
+    for (int i = 1; i <= n; i++) cnt[x[i] = s[i]]++;
+    for (int i = 1; i <= m; i++) cnt[i] += cnt[i - 1];
+    for (int i = n; i >= 1; i--) sa[cnt[x[i]]--] = i;
+    for (int k = 1; k <= n; k <<= 1) {
+        int num = 0;
+        for (int i = n - k + 1; i <= n; i++) y[++num] = i;
+        for (int i = 1; i <= n; i++) if (sa[i] > k) y[++num] = sa[i] - k;
+        for (int i = 1; i <= m; i++) cnt[i] = 0;
+        for (int i = 1; i <= n; i++) cnt[x[i]]++;
+        for (int i = 1; i <= m; i++) cnt[i] += cnt[i - 1];
+        for (int i = n; i >= 1; i--) sa[cnt[x[y[i]]]--] = y[i], y[i] = 0;
+        swap(x, y);
+        x[sa[1]] = 1, num = 1;
+        for (int i = 2; i <= n; i++) {
+            x[sa[i]] = (y[sa[i]] == y[sa[i - 1]] && y[sa[i] + k] == y[sa[i - 1] + k]) ? num : ++num;
+        }
+        if (num == n) break;
+        m = num;
     }
 }
-void exkmp(char *s, int n, char *t, int m) {
-    Z(t, m);
-    for (int i = 1, l = 0, r = 0; i <= n; i++) {
-        if (i <= r) p[i] = min(z[i - l + 1], r - i + 1);
-        while (i + p[i] <= n && s[i + p[i]] == t[p[i] + 1]) p[i]++;
-        if (i + p[i] - 1 > r) l = i, r = i + p[i] - 1;
+void get_height() {
+    int k = 0;
+    for (int i = 1; i <= n; i++) rk[sa[i]] = i;
+    for (int i = 1; i <= n; i++) {
+        if (rk[i] == 1) continue;
+        k = max(0, k - 1);
+        int j = sa[rk[i] - 1];
+        while (j + k <= n && i + k <= n && s[i + k] == s[j + k]) k++;
+        height[rk[i]] = k;
     }
 }
 signed main() {
-    scanf("%s%s", a + 1, b + 1);
-    n = strlen(a + 1), m = strlen(b + 1);
-    exkmp(a, n, b, m);
-    long long ans = 0;
-    for (int i = 1; i <= m; i++) ans ^= 1LL * i * (z[i] + 1);
-    printf("%lld\n", ans);
-    ans = 0;
-    for (int i = 1; i <= n; i++) ans ^= 1LL * i * (p[i] + 1);
-    printf("%lld\n", ans);
+    scanf("%s", s + 1);
+    get_SA();
+    for (int i = 1; i <= n; i++) printf("%d ", sa[i]);
     return 0;
 }
