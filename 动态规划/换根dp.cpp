7a97c1931bab40d6923ec849cfa037e8172089ed
youngk1019 <1186354278@qq.com>
2023-09-18 17:19:38 +0800

    增加换根dp注释

--- 动态规划/换根dp.cpp
+++ 动态规划/换根dp.cpp
@@ -1,4 +1,14 @@
 //codeforces CF1187E Tree Painting
+//给定一棵n个点的树 初始全是白点
+//要求你做n步操作，每一次选定一个与一个黑点相隔一条边的白点，将它染成黑点，然后获得该白点被染色前所在的白色联通块大小的权值
+//第一次操作可以任意选点
+//求可获得的最大权值
+//dp为总贡献, f为子贡献
+// dp[y] = n + (n - size_y) + sum_{i in son_x && i != y}f[i] + sum_{i in son_y}f[i] 
+// = n + (n - size_y) + sum_{i in son_x && i != y}f[i] + f[y] - size_y
+// = dp[x] + n - 2 * size_y
+// 第一次dfs计算出当1为根的dp结果，和中间变量
+// 第二次dfs先考虑计算所有的完整的中间变量的状态，然后变成从v为根的状态的中间变量，然后减去从v->u的代价 加上从u->v的代价
 #include <bits/stdc++.h>
 using namespace std;
 #define maxn 200100
